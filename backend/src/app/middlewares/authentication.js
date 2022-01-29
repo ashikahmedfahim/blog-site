fb01bf430602260181c87604071aea95a9fcc121
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2022-01-29 21:17:34 +0600

    authentication middleware added

---
+++ backend/src/app/middlewares/authentication.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const { ExpressError } = require("../utilities");
+
+module.exports.isAuthenticated = (req, res, next) => {
+  try {
+    const auth = req.header("Authorization");
+    if (!auth) throw new Error(`Token not found!`);
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) throw new ExpressError(401, "Invalid token");
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
