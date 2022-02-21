48fe3beaa4c23be23bfeb96334303dd328100cc9
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2022-02-21 14:03:00 +0600

    sign up form validation added

--- frontend/src/app/components/Form.js
+++ frontend/src/app/components/Form.js
@@ -1,49 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import AlertDialog from "./AlertDialog";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const Form = () => {
-  const [open, setOpen] = useState(false);
+  const SignUpschema = yup.object().shape({
+    firstName: yup.string().required("First name is required"),
+    lastName: yup.string().required("Last name is required"),
+    email: yup.string().email().required("Email is required"),
+    password: yup.string().min(8).required("Password is required"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match")
+      .required("Confirm Password is required"),
+  });
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(SignUpschema),
+  });
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  useEffect(() => {}, [open]);
+  const handleFormSubmit = (formData) => {
+    console.log(formData);
+  }
 
   return (
-    <Box component="form" noValidate autoComplete="off">
+    <Box
+      component="form"
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit(handleFormSubmit)}
+    >
       <Paper elevation={3} className="p-10 my-10">
         <TextField
           required
           fullWidth
           id="firstName"
           label="First Name"
-          helperText="Incorrect entry."
-          error
+          helperText={errors.firstName ? errors.firstName.message : ""}
+          error={errors.firstName ? true : false}
+          {...register("firstName")}
+          sx={{ marginBottom: "25px" }}
         />
         <TextField
           required
           fullWidth
           id="lastName"
           label="Last Name"
-          helperText="Incorrect entry."
-          error
+          helperText={errors.lastName ? errors.lastName.message : ""}
+          error={errors.lastName ? true : false}
+          {...register("lastName")}
+          sx={{ marginBottom: "25px" }}
         />
         <TextField
           required
           fullWidth
           id="email"
           label="Email"
-          helperText="Incorrect entry."
-          error
+          helperText={errors.email ? errors.email.message : ""}
+          error={errors.email ? true : false}
+          {...register("email")}
+          sx={{ marginBottom: "25px" }}
         />
         <TextField
           required
@@ -51,8 +76,10 @@ const Form = () => {
           id="password"
           label="Password"
           type="password"
-          helperText="Incorrect entry."
-          error
+          helperText={errors.password ? errors.password.message : ""}
+          error={errors.password ? true : false}
+          {...register("password")}
+          sx={{ marginBottom: "25px" }}
         />
         <TextField
           required
@@ -60,17 +87,21 @@ const Form = () => {
           id="confirmPassword"
           label="Confirm Password"
           type="password"
-          helperText="Incorrect entry."
-          error
+          helperText={
+            errors.confirmPassword ? errors.confirmPassword.message : ""
+          }
+          error={errors.confirmPassword ? true : false}
+          {...register("confirmPassword")}
+          sx={{ marginBottom: "25px" }}
         />
-        <Box className="py-5 flex justify-end">
+        <Box className="flex justify-end">
           <Box className="mx-5">
             <Button variant="contained" color="error">
               Back
             </Button>
           </Box>
           <Box>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" type="submit">
               Sign up
             </Button>
           </Box>
